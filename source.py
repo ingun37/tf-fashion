85ccc6b25a3c99f1865257f11664a3e19030aca4
ingun <ingun37@gmail.com>
2019-07-24 00:36:34 +0900

    predictions

--- source.py
+++ source.py
@@ -1,7 +1,42 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+# TensorFlow and tf.keras
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow import keras
 
-print(tf.version.VERSION)
-print(tf.keras.__version__)
+# Helper libraries
+import numpy as np
+# import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+
+print(predictions)
